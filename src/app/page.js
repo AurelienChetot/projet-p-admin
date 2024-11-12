c94898b676296595635d227ab4b861732adab3ce
AurelienChetot <aurelien.chetot@gmail.com>
2024-11-12 20:07:13 +0100

    add logo

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,20 @@
 import Image from "next/image";
 import ImgHome from "../app/assets/image/patricia.gif";
+import Logo from "../app/assets/image/logo.png";
 
 export default function Home() {
   return (
     <main>
-      <div className="content">
+      <section className="content">
+        <div className="logo-container">
+          <Image
+            className="img-logo"
+            src={Logo}
+            alt={Logo}
+            width={150}
+            height={150}
+          />
+        </div>
         <div className="img-text-content">
           <Image
             className="img-home"
@@ -16,13 +26,14 @@ export default function Home() {
           <p>
             Je suis ravie de vous accueillir sur mon site. En tant que
             psychopraticienne, je suis dévouée à accompagner mes clients dans
-            leur cheminement personnel vers le bien-être et l'équilibre mental.
+            leur cheminement personnel vers le bien-être et l'équilibre mental.{" "}
+            <br />
             Mon approche bienveillante et personnalisée vise à vous aider à
             surmonter les défis émotionnels et psychologiques que vous pouvez
             rencontrer.
           </p>
         </div>
-
+        <div className="border-bottom"></div>
         <p>
           Bataille's analysis of predialectic objectivism holds that culture is
           used in the service of archaic, sexist perceptions of art. A number of
@@ -52,7 +63,7 @@ export default function Home() {
           objectivism holds, we have to choose between predialectic objectivism
           and Lacanist obscurity.
         </p>
-      </div>
+      </section>
     </main>
   );
 }
